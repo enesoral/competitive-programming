63585c47d98b5a47e3af4db37b45b6804417fd96
enesoral <oralenes0@gmail.com>
2020-07-25 17:46:16 +0300

    Add 'greedy for money' solution

---
+++ Inzva Algorithm Competition/League 2/Contest 4/Greedy for Money/Solution.java
@@ -0,0 +1,161 @@
+/**
+ * Problem Link - https://www.hackerrank.com/contests/inzva-algorithm-competition-league-2-contest-4/challenges/greedy-for-money
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.InputMismatchException;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		InputReader in = new InputReader(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		Solver solver = new Solver();
+		solver.solve(in, out);
+		out.close();
+	}
+
+	static class Solver {
+
+		public void solve(InputReader in, PrintWriter out) {
+			int n = in.nextInt();
+			Integer[] arr = new Integer[n];
+
+			for (int i = 0; i < n; ++i) {
+				arr[i] = in.nextInt();
+			}
+
+			Arrays.sort(arr, Collections.reverseOrder());
+			int count = 0;
+			while (arr[0] >= n) {
+				arr[0] -= n;
+				for (int i = 1; i < n; ++i) {
+					arr[i] += 1;
+				}
+				Arrays.sort(arr, Collections.reverseOrder());
+				++count;
+			}
+			out.println(count);
+
+			// Alternative solution hint: Use priority queue
+		}
+	}
+
+	static class InputReader {
+
+		private InputStream stream;
+
+		private byte[] buf = new byte[1024];
+
+		private int curChar;
+
+		private int numChars;
+
+		private SpaceCharFilter filter;
+
+		public InputReader(InputStream stream) {
+			this.stream = stream;
+		}
+
+		private int read() {
+			if (numChars == -1)
+				throw new InputMismatchException();
+
+			if (curChar >= numChars) {
+				curChar = 0;
+				try {
+					numChars = stream.read(buf);
+				}
+				catch (IOException e) {
+					throw new InputMismatchException();
+				}
+
+				if (numChars <= 0)
+					return -1;
+			}
+			return buf[curChar++];
+		}
+
+		public int nextInt() {
+			int c = read();
+
+			while (isSpaceChar(c))
+				c = read();
+
+			int sgn = 1;
+
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+
+			int res = 0;
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c));
+
+			return res * sgn;
+		}
+
+		public long nextLong() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			long res = 0;
+
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c));
+			return res * sgn;
+		}
+
+		private String readString() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			}
+			while (!isSpaceChar(c));
+
+			return res.toString();
+		}
+
+		public boolean isSpaceChar(int c) {
+			if (filter != null)
+				return filter.isSpaceChar(c);
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+
+		public String next() {
+			return readString();
+		}
+
+		public interface SpaceCharFilter {
+
+			boolean isSpaceChar(int ch);
+
+		}
+	}
+}
