fd3c4bf2b21e889bca70eebeef42867d2717a6bf
enesoral <oralenes0@gmail.com>
2020-08-18 20:30:53 +0300

    Add 'stock trading 1' solution

---
+++ Inzva/Intermediate Training Set/Solution.java
@@ -0,0 +1,157 @@
+/**
+ * Problem Link - https://www.hackerrank.com/contests/inzva-intermediate-training-set/challenges/adding-all-together
+ *
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		InputReader in = new InputReader(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		Solver solver = new Solver();
+		solver.solve(in, out);
+		out.close();
+	}
+
+	static class Solver {
+
+		public void solve(InputReader in, PrintWriter out) {
+			int N = in.nextInt();
+			int max, min;
+			double decrease = -1, increase = -1;
+			max = min = in.nextInt();
+
+			for (int i = 1; i < N; ++i) {
+				int curr = in.nextInt();
+				if (curr < max) decrease = Math.max(decrease, (max - curr) * 100. / max);
+				if (curr > min) increase = Math.max(increase, (curr - min) * 100. / min);
+
+				max = Math.max(max, curr);
+				min = Math.min(min, curr);
+			}
+
+			out.println(decrease);
+			out.println(increase);
+		}
+
+	}
+
+	static class InputReader {
+
+		private InputStream stream;
+
+		private byte[] buf = new byte[1024];
+
+		private int curChar;
+
+		private int numChars;
+
+		private SpaceCharFilter filter;
+
+		public InputReader(InputStream stream) {
+			this.stream = stream;
+		}
+
+		private int read() {
+			if (numChars == -1)
+				throw new InputMismatchException();
+
+			if (curChar >= numChars) {
+				curChar = 0;
+				try {
+					numChars = stream.read(buf);
+				}
+				catch (IOException e) {
+					throw new InputMismatchException();
+				}
+
+				if (numChars <= 0)
+					return -1;
+			}
+			return buf[curChar++];
+		}
+
+		public int nextInt() {
+			int c = read();
+
+			while (isSpaceChar(c))
+				c = read();
+
+			int sgn = 1;
+
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+
+			int res = 0;
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c));
+
+			return res * sgn;
+		}
+
+		public long nextLong() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			long res = 0;
+
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c));
+			return res * sgn;
+		}
+
+		private String readString() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			}
+			while (!isSpaceChar(c));
+
+			return res.toString();
+		}
+
+		public boolean isSpaceChar(int c) {
+			if (filter != null)
+				return filter.isSpaceChar(c);
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+
+		public String next() {
+			return readString();
+		}
+
+		public interface SpaceCharFilter {
+
+			boolean isSpaceChar(int ch);
+
+		}
+	}
+}
