8881ae28721430d67d3cffe308ce812e2156363e
Muhammed Enes Oral <enesoral.seng@gmail.com>
2021-07-05 08:44:42 +0300

    Add 'reverse string in place' solution

---
+++ Others/Interview Cake/Array and string manipulation/Reverse String in Place/Solution.java
@@ -0,0 +1,190 @@
+/**
+ * Problem Link - https://www.interviewcake.com/question/java/reverse-string-in-place
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        Solver solver = new Solver();
+        solver.solve(in, out);
+        out.close();
+    }
+
+    static class Solver {
+        public void solve(InputReader in, PrintWriter out) {
+            int n = in.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = in.nextInt();
+            }
+
+            reverse(arr);
+
+            out.println(Arrays.toString(arr));
+        }
+
+        public static void reverse(int[] arrayOfChars) {
+            int i = 0;
+            int j = arrayOfChars.length - 1;
+
+            while (i < j) {
+                int temp = arrayOfChars[i];
+                arrayOfChars[i] = arrayOfChars[j];
+                arrayOfChars[j] = temp;
+
+                i++;
+                j--;
+            }
+        }
+
+        public static class Meeting {
+
+            private int startTime;
+            private int endTime;
+
+            public Meeting(int startTime, int endTime) {
+                // number of 30 min blocks past 9:00 am
+                this.startTime = startTime;
+                this.endTime   = endTime;
+            }
+
+            public int getStartTime() {
+                return startTime;
+            }
+
+            public void setStartTime(int startTime) {
+                this.startTime = startTime;
+            }
+
+            public int getEndTime() {
+                return endTime;
+            }
+
+            public void setEndTime(int endTime) {
+                this.endTime = endTime;
+            }
+        }
+    }
+
+    static class InputReader {
+
+        private InputStream stream;
+
+        private byte[] buf = new byte[1024];
+
+        private int curChar;
+
+        private int numChars;
+
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        private int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                }
+                catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+
+            while (isSpaceChar(c))
+                c = read();
+
+            int sgn = 1;
+
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        private String readString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return readString();
+        }
+
+        public interface SpaceCharFilter {
+
+            boolean isSpaceChar(int ch);
+
+        }
+    }
+}
