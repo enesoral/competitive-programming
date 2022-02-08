18fce9a5c66852ba7c41aa50a903f7089d8191ff
Muhammed Enes Oral <enesoral.seng@gmail.com>
2022-02-08 17:03:28 +0300

    Add 'urlify' solution

---
+++ Others/Cracking the Coding Interview/1- Arrays and Strings/3- URLify/Solution.java
@@ -0,0 +1,163 @@
+/**
+ * Problem Link - https://www.interviewcake.com/question/java/reverse-string-in-place
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        Solver solver = new Solver();
+        solver.solve(in, out);
+        out.close();
+    }
+
+    static class Solver {
+        public void solve(InputReader in, PrintWriter out) {
+            String str = "Mr John Smith    ";
+            int realLength = 13;
+
+            char[] chars = str.toCharArray();
+            int spaceCount = 0;
+            for (int i = 0; i < realLength; i++) {
+                if (chars[i] == ' ') spaceCount++;
+            }
+
+            int lastIndex = realLength + spaceCount * 2;
+            lastIndex--;
+
+            for (int i = realLength - 1; i >= 0; i--) {
+                if (chars[i] == ' ') {
+                    chars[lastIndex] = '0';
+                    chars[lastIndex - 1] = '2';
+                    chars[lastIndex - 2] = '%';
+                    lastIndex -= 3;
+                } else {
+                    chars[lastIndex] = chars[i];
+                    lastIndex--;
+                }
+            }
+
+            out.println(chars);
+        }
+    }
+
+    static class InputReader {
+
+        private InputStream stream;
+
+        private byte[] buf = new byte[1024];
+
+        private int curChar;
+
+        private int numChars;
+
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        private int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                }
+                catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+
+            while (isSpaceChar(c))
+                c = read();
+
+            int sgn = 1;
+
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        private String readString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return readString();
+        }
+
+        public interface SpaceCharFilter {
+
+            boolean isSpaceChar(int ch);
+
+        }
+    }
+}
