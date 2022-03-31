d88d5f8eb687fb6acd86b9359a7bbf121dab9540
Muhammed Enes Oral <enesoral.seng@gmail.com>
2022-03-31 22:20:33 +0300

    Add 'minimal tree' solution

---
+++ Others/Cracking the Coding Interview/4- Trees and Graphs/2- Minimal Tree/Solution.java
@@ -0,0 +1,174 @@
+/**
+ * Problem: Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a
+ *          binary search tree with minimal height.
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        Solver solver = new Solver();
+        solver.solve(in, out);
+        out.close();
+    }
+
+    static class Solver {
+        public void solve(InputReader in, PrintWriter out) {
+            int n = in.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = in.nextInt();
+            }
+            Arrays.sort(arr);
+            out.println(createMinimalBST(arr, 0, n - 1));
+        }
+
+        public TreeNode createMinimalBST(int[] arr, int start, int end) {
+            if (end < start) {
+                return null;
+            }
+
+            TreeNode node = new TreeNode();
+            int mid = (start + end) / 2;
+            node.data = arr[mid];
+            node.left = createMinimalBST(arr, start, mid - 1);
+            node.right = createMinimalBST(arr, mid + 1, end);
+            return node;
+        }
+    }
+
+    static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "data=" + data +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
+    }
+
+    static class InputReader {
+
+        private InputStream stream;
+
+        private byte[] buf = new byte[1024];
+
+        private int curChar;
+
+        private int numChars;
+
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        private int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+
+            while (isSpaceChar(c))
+                c = read();
+
+            int sgn = 1;
+
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        private String readString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return readString();
+        }
+
+        public interface SpaceCharFilter {
+
+            boolean isSpaceChar(int ch);
+
+        }
+    }
+}
