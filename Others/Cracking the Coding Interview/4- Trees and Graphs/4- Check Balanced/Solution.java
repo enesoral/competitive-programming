bfc3cef4fcff08d9c61c0b5acb8d337aaabea144
Muhammed Enes Oral <enesoral.seng@gmail.com>
2022-04-07 19:20:50 +0300

    Add 'check balanced' solution

---
+++ Others/Cracking the Coding Interview/4- Trees and Graphs/4- Check Balanced/Solution.java
@@ -0,0 +1,197 @@
+/**
+ * Problem: Check if a binary tree is balanced. A balanced tree is defined to be a tree such that
+ *          the heights of the two subtrees of any node never differ by more than one.
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        Solver solver = new Solver();
+        solver.solve(in, out);
+        out.close();
+    }
+
+    static class Solver {
+        public void solve(InputReader in, PrintWriter out) {
+            TreeNode node1 = new TreeNode(1);
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            TreeNode node4 = new TreeNode(4);
+            TreeNode node5 = new TreeNode(5);
+            TreeNode node6 = new TreeNode(6);
+            TreeNode node7 = new TreeNode(7);
+            TreeNode node8 = new TreeNode(8);
+
+            node4.left = node8;
+            node2.left = node4;
+//            node2.right = node5;
+//            node3.left = node6;
+//            node3.right = node7;
+            node1.left = node2;
+            node1.right = node3;
+
+            out.println(isBalanced(node1));
+        }
+
+        public boolean isBalanced(TreeNode root) {
+            return checkHeight(root) != Integer.MIN_VALUE;
+        }
+
+        public int checkHeight(TreeNode root) {
+            if (root == null) return -1;
+
+            int leftHeight = checkHeight(root.left);
+            if (leftHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+            int rightHeight = checkHeight(root.right);
+            if (rightHeight == Integer.MAX_VALUE) return Integer.MAX_VALUE;
+
+            if(Math.abs(leftHeight - rightHeight) > 1) {
+                return Integer.MIN_VALUE;
+            } else {
+                return Math.max(leftHeight, rightHeight) + 1;
+            }
+        }
+    }
+
+    static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int data) {
+            this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "data=" + data +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
+    }
+
+    static class InputReader {
+
+        private InputStream stream;
+
+        private byte[] buf = new byte[1024];
+
+        private int curChar;
+
+        private int numChars;
+
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        private int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+
+            while (isSpaceChar(c))
+                c = read();
+
+            int sgn = 1;
+
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        private String readString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return readString();
+        }
+
+        public interface SpaceCharFilter {
+
+            boolean isSpaceChar(int ch);
+
+        }
+    }
+}
